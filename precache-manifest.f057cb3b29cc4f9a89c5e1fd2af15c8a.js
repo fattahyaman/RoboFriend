f97f43e221077a35979523979589ebfe29c5ef89
maniyamman <maniyaman76@gmail.com>
2020-04-08 10:01:50 +0200

    Updates

---
+++ precache-manifest.f057cb3b29cc4f9a89c5e1fd2af15c8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9df81b9bd0e08a6225d99300f2fea3c5",
+    "url": "/fattahyaman/RoboFriend/index.html"
+  },
+  {
+    "revision": "e733ed7cfee4751c10b9",
+    "url": "/fattahyaman/RoboFriend/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "4b322e989f075eca866a",
+    "url": "/fattahyaman/RoboFriend/static/css/main.9d3f00a4.chunk.css"
+  },
+  {
+    "revision": "e733ed7cfee4751c10b9",
+    "url": "/fattahyaman/RoboFriend/static/js/2.ced7e3ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fattahyaman/RoboFriend/static/js/2.ced7e3ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b322e989f075eca866a",
+    "url": "/fattahyaman/RoboFriend/static/js/main.2769879d.chunk.js"
+  },
+  {
+    "revision": "e7374d891dbe9eb594aa",
+    "url": "/fattahyaman/RoboFriend/static/js/runtime-main.12a73a91.js"
+  }
+]);
